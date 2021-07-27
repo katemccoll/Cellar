ade38da79a06fe751f410084e64ccd114a0bf19d
Kate McColl <kate@katemccollart.com>
2021-07-28 11:29:19 +1200

    add: wine model; user and wine to index

---
+++ server/models/Wine.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const wineSchema = new Schema({
+    wineId: {
+
+    },
+    wineName: {
+        type: String,
+        required: 'You need to leave a winery name',
+        trim: true,
+    },
+    wineType: {
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    },
+    comments: [
+        {
+            commentText: {
+                type: String,
+                required: true,
+                minlength: 1,
+                maxlength: 280,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now,
+                get: (timestamp) => dateFormat(timestamp),
+            },
+        },
+    ],
+    // ratings: [
+    //     {
+    //         rateWine: {
+    //             type: Number,
+    //             default: 0,
+    //         },
+    //         rateValue: {
+    //             type: Number,
+    //             default: 0,
+    //         },
+    //     },
+    // ],
+});
+
+const Wine = model('Wine', wineSchema);
+
+module.exports= Wine;
---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const User = require('./User');
+const Wine = require('./Wine')
+
+module.exports = { User, Wine };
