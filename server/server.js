37ea3a8cf4f9a70e5df8bdb54fa6978c0faf01d6
Kate McColl <kate@katemccollart.com>
2021-07-28 21:24:26 +1200

    change: port to 3000

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -17,13 +17,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// }
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
     app.listen(PORT, () => {
@@ -32,3 +32,5 @@ db.once('open', () => {
         console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
 });
+
+
