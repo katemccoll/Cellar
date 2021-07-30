68759a19b9ec611835034d86f9e0719627820dd0
Kate McColl <kate@katemccollart.com>
2021-07-30 17:15:00 +1200

    add: navbar with styling and buttons

---
+++ client/src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'navbar-links'
+    },
+    {
+        title: 'Add Wine',
+        url: '#',
+        cName: 'navbar-links'
+    },
+    {
+        title: 'Search',
+        url: '#',
+        cName: 'navbar-links'
+    },
+    {
+        title: 'Sign In',
+        url: '#',
+        cName: 'navbar-links-mobile'
+    },
+]
