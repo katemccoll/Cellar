4b35068bd929eb35170895d9a28839fc6d2f927a
Kate McColl <kate@katemccollart.com>
2021-07-31 09:17:50 +1200

    add: hero section with video

---
+++ client/src/components/HeroSection/HeroSection.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Button} from "../Button/Button";
+import "./HeroSection.css"
+
+function HeroSection() {
+    return (
+        <div className="hero-container">
+            <video src="/videos/pexels-kindel-media-8093236.mp4" autoPlay loop muted></video>
+            <h1>Cellar</h1>
+            <p>Your Wine Bank</p>
+            <div className="hero-btns">
+                <Button className="btn" buttonSize="btn--large" buttonStyle="btn--outline">Add Wine</Button>
+                <Button className="btn" buttonSize="btn--large" buttonStyle="btn--outline">View Your Wines</Button>
+            </div>
+        </div>
+    );
+}
+
+export default HeroSection;
