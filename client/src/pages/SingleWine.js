b1d4fc4aa22f84eeb96a45513bb668f689acea11
Kate McColl <kate@katemccollart.com>
2021-08-03 09:09:19 +1200

    add: single wine layout

---
+++ client/src/pages/SingleWine.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import {QUERY_SINGLE_WINE} from "../utils/queries";
+
+import { useQuery } from '@apollo/client';
+
+const SingleWine = () => {
+    const { wineId } = useParams();
+
+    const { loading, data } = useQuery(QUERY_SINGLE_WINE, {
+        variables: { wineId: wineId },
+    });
+
+    const wine = data?.wine || {};
+    if (loading) {
+        return <div>Loading...</div>
+    }
+    return (
+        <>
+            <div className="background-bottles">
+                <div className="card-add-wine">
+                    <h1>
+                        {wine.wineName}
+                    </h1>
+                    <h2>
+                        {wine.wineType}
+                    </h2>
+                    <div className="upload-image">Upload</div>
+                    <div>
+                        {wine.wineText}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default SingleWine;
