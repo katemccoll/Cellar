a83c19257cbce733631aaacb8bf9abc91afc75bf
Kate McColl <kate@katemccollart.com>
2021-08-03 22:48:46 +1200

    add: errors and validators

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -3,16 +3,70 @@ import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
 import Auth from '../utils/auth';
 import "./assets/css/Signup.css";
-import logo from "./assets/images/cellar-logo-small.png";
-// import {Button} from "./Button/Button";
+import validator from "validator";
+import {Button} from "./Button/Button";
+
+
 
 const Signup = (props) => {
     const [formState, setFormState] = useState({
-        username: '',
+        firstName: '',
+        lastName: '',
         email: '',
         password: '',
     });
     const [addUser] = useMutation(ADD_USER);
+    const [errors, setErrors] = useState({});
+
+    const ValidateInfo = (values) => {
+        let errors = {};
+
+        if (!values.firstName) {
+            errors.firstName = "First name is required";
+        }
+
+        if (!values.lastName) {
+            errors.lastName = "Last name is required";
+        }
+
+        if (!values.email) {
+            errors.email = "Email is required";
+        } else if (!validator.isEmail(values.email)) {
+            errors.email = "Email is invalid";
+        }
+
+        if (!values.password) {
+            errors.password = "Password is required";
+        } else if (values.password.length < 8) {
+            errors.password = "Password needs to be 8 characters or longer";
+        }
+
+        if (!values.confirmPassword) {
+            errors.confirmPassword = "Password is required";
+        } else if (values.confirmPassword !== values.password) {
+            errors.confirmPassword = "Passwords need to match";
+        }
+        return errors;
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        setErrors(ValidateInfo(formState));
+        try {
+            const mutationResponse = await addUser({
+                variables: {
+                    firstName: formState.firstName,
+                    lastName: formState.lastName,
+                    email: formState.email,
+                    password: formState.password
+                },
+            });
+            const token = mutationResponse.data.addUser.token;
+            Auth.login(token);
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -23,24 +77,9 @@ const Signup = (props) => {
         });
     };
 
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-
-        const mutationResponse = await addUser({
-            variables: {
-                firstName: formState.firstName,
-                lastName: formState.lastName,
-                email: formState.email,
-                password: formState.password
-            },
-        });
-        const token = mutationResponse.data.addUser.token;
-        Auth.login(token);
-    };
-
     return (
         <div className="card-h">
-            <div className="t-a-center"><img src={logo} alt="cellar-logo" className="logo" /></div>
+
             <div className="input-group">
                 <form onSubmit={handleFormSubmit}>
                     <input
@@ -51,6 +90,7 @@ const Signup = (props) => {
 
                         onChange={handleChange}
                     />
+                    {errors.firstName && <p className="error-message">{errors.firstName}</p>}
                     <input
                         type="text"
                         name="lastName"
@@ -58,6 +98,7 @@ const Signup = (props) => {
                         placeholder="Last Name"
                         onChange={handleChange}
                     />
+                    {errors.lastName && <p className="error-message">{errors.lastName}</p>}
                     <input
                         type="email"
                         name="email"
@@ -65,6 +106,7 @@ const Signup = (props) => {
                         placeholder="Email"
                         onChange={handleChange}
                     />
+                    {errors.email && <p className="error-message">{errors.email}</p>}
                     <input
                         type="password"
                         name="password"
@@ -72,7 +114,23 @@ const Signup = (props) => {
                         placeholder="Password"
                         onChange={handleChange}
                     />
-                    <button className="btn" type="submit" >Sign Up</button>
+                    {errors.password && <p className="error-message">{errors.password}</p>}
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        className="signup-input"
+                        placeholder="Confirm Password"
+                        onChange={handleChange}
+                    />
+                    {errors.confirmPassword && <p className="error-message">{errors.confirmPassword}</p>}
+                    <Button
+                        type={"submit"}
+                        className="btn"
+                        sizebutton="btn--large"
+                        stylebutton="btn--outline"
+                    >
+                        Sign Up
+                    </Button>
                 </form>
             </div>
         </div>
