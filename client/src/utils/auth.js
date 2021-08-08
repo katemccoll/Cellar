c9dbf32de9290ca69786eaff20c58fbe9519ca3a
Kate McColl <kate@katemccollart.com>
2021-08-09 09:35:56 +1200

    fix: logout route

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -22,7 +22,7 @@ class AuthService {
 
     logout() {
         localStorage.removeItem('id_token');
-        window.location.reload();
+        window.location.assign('/');
     }
 
     loggedIn() {
