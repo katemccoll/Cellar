7c4c8efdc0a4feecd284525761cf1b20cf957029
Kate McColl <kate@katemccollart.com>
2021-08-08 23:59:34 +1200

    add: images

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,9 @@ import React from 'react';
 import "../App.css";
 import HeroSection from "../components/HeroSection/HeroSection";
 import "./assets/css/Home.css";
+import GrapeIcon from "../assets/images/grape-icon-kate.png";
+import WineGlassIcon from "../assets/images/wine-glass-kate.png";
+import BottleIcon from "../assets/images/bottle-icon-kate.png";
 
 const Home = () => {
 
@@ -14,20 +17,26 @@ const Home = () => {
                         Never forget a wine again!
                     </h2>
                     <p>
-                        Are you also taking way too much time looking at wine trying to remember which one you tried and liked?
-                        Or which one your partner hated? Or even scrolling through your photos to find that wine you tried at
+                        Are you also taking way too long looking at wine trying to remember which one you tried and liked?
+                        Or which one your mother hated so you don't take that one to Sunday night dinner? Or even scrolling through your photos to find that wine you tried at
                         that restaurant that one  We have all been there!
                     </p>
                 </div>
-                <div>
+                <div className="about-icons">
                     <div>
+                        <img src={GrapeIcon} alt={"grapes"}/>
                         <h4>Have a wine you love?</h4>
+                        <p>Tried a wine at a restaurant that you loved?</p>
                     </div>
-                    <div>
+                    <div className="wine-icon">
+                        <img src={WineGlassIcon} alt={"wine-glass"}/>
                         <h4>Add the wine</h4>
+                        <p>Open up Cellar and easily add information about the wine</p>
                     </div>
                     <div>
+                        <img src={BottleIcon} alt={"bottle"}/>
                         <h4>Time Saver!</h4>
+                        <p>Quickly find the wine you want to try the nect time you are at the supermarket or liquor store.</p>
                     </div>
                 </div>
 
