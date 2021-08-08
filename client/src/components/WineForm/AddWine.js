4832fee1e6290129c7ab8b6218a14f2015aad11d
Kate McColl <kate@katemccollart.com>
2021-08-08 14:58:25 +1200

    fix: stying on add wine page

--- client/src/components/WineForm/AddWine.js
+++ client/src/components/WineForm/AddWine.js
@@ -7,7 +7,6 @@ import { useMutation } from "@apollo/client";
 import { ADD_WINE } from "../../utils/mutations";
 import { QUERY_WINES } from "../../utils/queries";
 
-
 const AddWine = () => {
     const [formState, setFormState] = useState({ wineryName: '', wineType: 'red-wine', description: '' });
     const [addWine, { error }] = useMutation(ADD_WINE, {
@@ -57,44 +56,56 @@ const AddWine = () => {
 
     return (
         <>
+            <div className="image-bottles">
+                <h1>Wine Diary Entry</h1>
+            </div>
             <div className="background-bottles">
                 <div className="card-add-wine">
-                    <h1>Wine Diary Entry</h1>
-                    <div className="upload-image">Upload</div>
-                    <input type="file" />
+
                     <form onSubmit={handleFormSubmit}>
-                        <label>
-                            Winery Name:
-                            <input className="input-add-wine" type="text" name="wineryName"
-                                   onChange={handleFormChange}/>
-                        </label>
-                        <label>
-                            Type of Wine:
-                            <select className="select-add-wine" name="wineType"
-                                    onChange={handleFormChange}>
-                                <option defaultValue="red-wine">Red Wine</option>
-                                <option value="white-wine">White Wine</option>
-                                <option value="rose-wine">Rosé Wine</option>
-                                <option value="sparkling-wine">Sparkling Wine</option>
-                                <option value="dessert-wine">Dessert Wine</option>
-                                <option value="fortified-wine">Fortified Wine</option>
-                            </select>
-                        </label>
-                            <label>Rating:
-                            <Rating name="rating"
-                                    className="rating"
-                                    size="large"
-                                    defaultValue={2}
-                                    precision={0.5}
-                                    emptyIcon={<StarBorderIcon fontSize="inherit" />}
-                                    onChange={handleFormChange}
-                                    />
+                        <div className="upload-container">
+                            <div className="upload-image">Upload</div>
+                            <input type="file" className="upload-image-file" />
+                        </div>
+
+                        <div className="name-container">
+                            <label>
+                                Winery Name:
+                                <input className="input-add-wine" type="text" name="wineryName"
+                                       onChange={handleFormChange}/>
                             </label>
-                        <label>
-                            Thoughts on the wine?
-                            <textarea className="textarea-add-wine" name="description"
-                                      onChange={handleFormChange}/>
-                        </label>
+                            <label>
+                                Type of Wine:
+                                <select className="select-add-wine" name="wineType"
+                                        onChange={handleFormChange}>
+                                    <option defaultValue="red-wine">Red Wine</option>
+                                    <option value="white-wine">White Wine</option>
+                                    <option value="rose-wine">Rosé Wine</option>
+                                    <option value="sparkling-wine">Sparkling Wine</option>
+                                    <option value="dessert-wine">Dessert Wine</option>
+                                    <option value="fortified-wine">Fortified Wine</option>
+                                </select>
+                            </label>
+                        </div>
+                        <div className="wine-text-container">
+                            <label>Rating:
+                                <Rating name="rating"
+                                        className="rating"
+                                        size="large"
+                                        defaultValue={2}
+                                        precision={0.5}
+                                        emptyIcon={<StarBorderIcon fontSize="inherit" />}
+                                        onChange={handleFormChange}
+                                />
+                            </label>
+                            <label>
+                                Thoughts on the wine?
+                                <textarea className="textarea-add-wine" name="description"
+                                          onChange={handleFormChange}/>
+                            </label>
+                        </div>
+
+
                         <div className="text-align-center">
                             <Button type="submit" className="btn" sizebutton="btn--large"
                                     stylebutton="btn--outline">Add</Button>
