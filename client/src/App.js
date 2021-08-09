6abd6ef60ff385aa89fedf7b7a8edd7490091c62
Kate McColl <kate@katemccollart.com>
2021-08-09 16:29:10 +1200

    fix: api url

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ import './App.css';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: process.env.GRAPHQL_API || 'http://localhost:3001/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
