185466b0772aa4de46e4bf2b31fb33cd29e07140
Kate McColl <kate@katemccollart.com>
2021-08-09 17:19:16 +1200

    add: star rating to cards

--- client/src/components/WineCard/WineCard.js
+++ client/src/components/WineCard/WineCard.js
@@ -2,12 +2,17 @@ import React from 'react';
 import {Link } from 'react-router-dom'
 import './WineCard.css';
 
+const MAX_STARS = 5;
+
 
 const WineCard = ({ wine }) => {
 
     const wineImage = wine.image == null ? {} :  {
         backgroundImage: 'url(' + wine.image + ')'
     };
+
+    const numberStars = wine.rating > 0 && wine.rating <= MAX_STARS ? Math.floor(MAX_STARS * wine.rating) : 0;
+    const ratingString = numberStars > 0 ? '★'.repeat(numberStars) : '-';
 
     return (
         <div className="wine-card-container">
@@ -18,6 +23,9 @@ const WineCard = ({ wine }) => {
                     </div>
                     <div className="wine-card-name">
                         <p className="capitalize">{wine.wineryName}</p>
+                    </div>
+                    <div>
+                        {ratingString}
                     </div>
                 </Link>
             </div>
